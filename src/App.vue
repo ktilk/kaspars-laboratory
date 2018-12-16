8c66376ac8811a11f0e7245cfff6564e5f206327
Kaspar Tilk <kaspar.tilk@itcollege.ee>
2018-12-16 21:43:47 +0200

    Initial commit: added top navbar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="sticky-container">
+      <div class="title-bar sticky is-stuck is-at-top">
+        <div class="title-bar-left">
+          <span>LOGO</span>
+          <router-link class="button" to="/">Home</router-link>
+          <router-link class="button" to="/about">About</router-link>
+        </div>
+        <div class="title-bar-right">
+          <a href="https://www.facebook.com/kaspar.tilk" class="social-logo social-logo-fb"></a>
+          <a href="https://twitter.com/KasparTilk" class="social-logo social-logo-twitter"></a>
+          <a href="https://www.linkedin.com/in/kaspartilk" class="social-logo social-logo-linkedin"></a>
+        </div>
+      </div>
     </div>
-    <router-view/>
+    <div class="body-content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+@import "~foundation-sites/scss/foundation.scss";
+@import "_settings.scss";
+@include foundation-button;
+@include foundation-title-bar;
+@include foundation-sticky;
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -16,14 +38,67 @@
   text-align: center;
   color: #2c3e50;
 }
-#nav {
-  padding: 30px;
+.title-bar {
+  padding-top: 0.5rem;
+  padding-bottom: 0;
+  box-shadow: 0 2px 5px 0 rgba(0,0,0,0.16), 0 2px 10px 0 rgba(0,0,0,0.12);
+
   a {
+    text-decoration: none;
     font-weight: bold;
-    color: #2c3e50;
+    color: white;
+    border-bottom: 2px solid transparent;
+
     &.router-link-exact-active {
-      color: #42b983;
+      border-bottom: 2px solid white;
     }
   }
+
+  .button {
+    margin: 0;
+    background-color: transparent;
+  }
+
+  .social-logo {
+    height: 24px;
+    width: 24px;
+    margin-left: 0.5rem;
+    padding: 0;
+    background-size: cover;
+    background-repeat: no-repeat;
+    display: inline-block;
+
+    &-fb {
+      background-image: url("./assets/flogo-HexRBG-Wht-58.svg");
+
+      &:hover {
+        background-image: url("./assets/f-ogo_RGB_HEX-58.svg");
+      }
+    }
+
+    &-twitter {
+      background-image: url("./assets/Twitter_Social_Icon_Rounded_Square_White.svg");
+
+      &:hover {
+        background-image: url("./assets/Twitter_Social_Icon_Rounded_Square_Color.svg");
+      }
+    }
+
+    &-linkedin {
+      background-image: url("./assets/In-White-28px-R.png");
+
+      &:hover {
+        background-image: url("./assets/In-2CRev-28px-R.png");
+      }
+    }
+  }
+
+  .title-bar-right {
+    text-align: center;
+  }
+}
+
+.body-content {
+  padding-top: 50px;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home page</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+// import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'home',
   components: {
-    HelloWorld,
   },
 };
 </script>
+
+<style lang="scss">
+  .home {
+    height: 1000px;
+  }
+</style>
